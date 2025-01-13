f8996b4be5a5b5d3725d50c2debf2b02dff6140c
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-01-13 14:10:03 -0300

    new models folder & update user routes
    new model of users & imported by usersRoutes.js for CRUD operations

--- backend/routes/api/usersRoutes.js
+++ backend/routes/api/usersRoutes.js
@@ -1,14 +1,63 @@
 const express = require('express')
 const router = express.Router()
+const { UserModel } = require('../../models/userModel')
+//api/users
 
-//get all users
-router.get('/', (req, res) => {
-    res.send("hello..")
+
+//DESC      get all users
+router.get('/', async (req, res) => {
+    const users = await UserModel.find()
+    return res.status(200).json(users)
 })
 
-//Get user by id
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+
+//DESC      Get user by id
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    try {
+        if(!user){
+            return res.status(404).json({msg: 'User Not Found'});
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
+    }
+})
+
+//DESC      post a new user
+router.post('/', async (req, res) => {
+    const newUser = new UserModel({...req.body})
+    const insertedUser = await newUser.save()
+    return res.status(201).json(insertedUser)
+})
+
+
+//update user by id
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const data = await UserModel.findByIdAndUpdate(id, req.body, {new:true})
+        if(!data){
+            return res.status(404).json({msg: `User  with id ${id} not found for update`})
+        }
+        else return res.send(data)
+    } catch (error) {
+        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
+    }
+})
+
+//delete user by id
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const data = await UserModel.findByIdAndDelete(id)
+        if(!data){
+            return res.status(404).json(`User with id ${id} not found for delete`)
+        } else return res.send(data)
+    } catch (error) {
+        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
+    }
 })
 
 module.exports = router
