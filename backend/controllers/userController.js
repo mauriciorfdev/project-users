4c0c411660d85e273a6e213a55978da115b98e0f
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-01-13 20:17:51 -0300

    new controllers folder
    move functions from routes to controller, and assosiate them using the router object

---
+++ backend/controllers/userController.js
@@ -0,0 +1,84 @@
+const { UserModel } = require('../models/userModel')
+
+
+//DESC      get users
+async function getUsers(req, res) {
+    try {
+        const users = await UserModel.find();
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json({ msg: 'Internal Server Error', error: error.message });
+    }
+}
+
+
+//DESC      get user by id
+async function getSingleUser(req, res) {
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    try {
+        if (!user) {
+            return res.status(404).json({ msg: 'User Not Found' });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({ msg: 'Internal Server Error', error: error.message });
+    }
+}
+
+
+//DESC      post user
+async function setUser(req, res) {
+    try {
+        const { name } = req.body;
+        if (!name) {
+            return res.status(400).json({ msg: 'Bad Request: name not found or empty' });
+        }
+        const newUser = new UserModel({ name });
+        const insertedUser = await newUser.save();
+        return res.status(201).json({ inserted: insertedUser });
+    } catch (error) {
+        return res.status.json({ msg: 'Internal Server Error', error: error.message });
+    }
+}
+
+
+//DESC      update user
+async function updateUser(req, res) {
+    const id = req.params.id;
+    const { name } = req.body;
+    try {
+        if (!name) {
+            return res.status(400).json({ msg: 'Bad Request name not found or empty' });
+        }
+        const data = await UserModel.findByIdAndUpdate(id, { name }, { new: true });
+        if (!data) {
+            return res.status(404).json({ msg: `User  with id ${id} not found for update` });
+        }
+        else return res.send(data);
+    } catch (error) {
+        return res.status(500).json({ msg: 'Internal Server Error', error: error.message });
+    }
+}
+
+
+//DESC      delete user
+async function deleteUser(req, res) {
+    const { id } = req.params;
+    try {
+        const data = await UserModel.findByIdAndDelete(id);
+        if (!data) {
+            return res.status(404).json(`User with id ${id} not found for delete`);
+        } else return res.send(data);
+    } catch (error) {
+        return res.status(500).json({ msg: 'Internal Server Error', error: error.message });
+    }
+}
+
+module.exports = {
+    getUsers,
+    getSingleUser,
+    setUser,
+    updateUser,
+    deleteUser,
+}
--- backend/routes/api/usersRoutes.js
+++ backend/routes/api/usersRoutes.js
@@ -1,75 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const { UserModel } = require('../../models/userModel')
+
+const { getUsers, 
+    getSingleUser,
+    setUser,
+    updateUser,
+    deleteUser,
+ } = require('../../controllers/userController')
 //api/users
 
 
-//DESC      get all users
-router.get('/', async (req, res) => {
-    const users = await UserModel.find()
-    return res.status(200).json(users)
-})
+//get all users
+router.get('/', getUsers)
 
+//get user by id
+router.get('/:id', getSingleUser)
 
-//DESC      Get user by id
-router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const user = await UserModel.findById(id);
-    try {
-        if(!user){
-            return res.status(404).json({msg: 'User Not Found'});
-        }
-        return res.status(200).json(user);
-    } catch (error) {
-        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
-    }
-})
-
-//DESC      post a new user
-router.post('/', async (req, res) => {
-    try {
-        const { name } = req.body
-        if (!name){
-            return res.status(400).json({msg: 'Bad Request: name not found'})
-        }
-        const newUser = new UserModel({ name })
-        const insertedUser = await newUser.save()
-        return res.status(201).json({inserted: insertedUser})
-    } catch (error) {
-        return res.status.json({msg: 'Internal Server Error', error: error.message})
-    }
-})
-
+//post a new user
+router.post('/', setUser)
 
 //update user by id
-router.put('/:id', async (req, res) => {
-    const id = req.params.id
-    const { name } = req.body
-    try {
-        if (!name){
-            return res.status(400).json({msg: 'Bad Request name not found'})
-        }
-        const data = await UserModel.findByIdAndUpdate(id, {name}, {new:true})
-        if(!data){
-            return res.status(404).json({msg: `User  with id ${id} not found for update`})
-        }
-        else return res.send(data)
-    } catch (error) {
-        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
-    }
-})
+router.put('/:id', updateUser)
 
 //delete user by id
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await UserModel.findByIdAndDelete(id)
-        if(!data){
-            return res.status(404).json(`User with id ${id} not found for delete`)
-        } else return res.send(data)
-    } catch (error) {
-        return res.status(500).json({msg: 'Internal Server Error', error: error.message});
-    }
-})
+router.delete('/:id', deleteUser)
 
 module.exports = router
