b002feba12de2e4245d14b364aadae02a52ef966
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-05-16 18:32:14 -0400

    update UsersList
    wrap the content using a Container

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -1,76 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import Table from 'react-bootstrap/Table'
-import Button from 'react-bootstrap/Button'
-import { useNavigate } from 'react-router-dom'
-import Spinner from 'react-bootstrap/Spinner'
+import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
 
 const UsersList = () => {
-
   const [users, setUsers] = useState([]);
-  const [isDataLoading, setIsDataLoading] = useState(false)
+  const [isDataLoading, setIsDataLoading] = useState(false);
   const navigate = useNavigate();
 
   const getUsers = async () => {
     try {
-      setIsDataLoading(true)
+      setIsDataLoading(true);
       const resp = await fetch('http://localhost:5000/api/users');
       const data = await resp.json();
-      console.log(data)
+      console.log(data);
       setUsers(data);
-      setIsDataLoading(false)
+      setIsDataLoading(false);
     } catch (error) {
-      console.error('error while fetching users: ', error.message)
+      console.error('error while fetching users: ', error.message);
     }
-  }
+  };
 
-  useEffect( () => {
+  useEffect(() => {
     getUsers();
-  }, [] )
+  }, []);
 
-  function handleUpdate(userId){
-    navigate(`/user/${userId}`)
+  function handleUpdate(userId) {
+    navigate(`/user/${userId}`);
   }
 
-  async function handleDelete(userId){
+  async function handleDelete(userId) {
     try {
-      const resp = await fetch(`http://localhost:5000/api/users/${userId}`,{
-        method: 'DELETE'
-      })
+      const resp = await fetch(`http://localhost:5000/api/users/${userId}`, {
+        method: 'DELETE',
+      });
       const data = await resp.json();
-      console.log(data)
+      console.log(data);
       getUsers();
     } catch (error) {
-      console.error("Error while deleting users: ", error.message)
+      console.error('Error while deleting users: ', error.message);
     }
   }
 
-  return (<>
-    <h2>Users List</h2>
+  return (
+    <>
+      <Container className='mt-4'>
+        <h2>Users List</h2>
 
-    {isDataLoading && <Spinner/> }
+        {isDataLoading && <Spinner />}
 
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Action</th>
-        </tr>
-      </thead>  
-      <tbody>
-        {users.map(user => 
-          <tr key={user._id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>
-              <Button variant='dark' onClick={() => handleUpdate(user._id)}>Update</Button>{' '}
-              <Button variant='danger' onClick={() => handleDelete(user._id)}>Delete</Button>
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </Table>
-  </>)
-}
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Button variant='dark' onClick={() => handleUpdate(user._id)}>
+                    Update
+                  </Button>{' '}
+                  <Button
+                    variant='danger'
+                    onClick={() => handleDelete(user._id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
+};
 
-export default UsersList
+export default UsersList;
