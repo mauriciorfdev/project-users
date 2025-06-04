ee9fd43693e115519dfb5eeaaf7ce4885bcda73c
m <139261114+mauriciorfdev@users.noreply.github.com>
2025-06-04 17:53:30 -0400

    update NavBar
    update NavBar: change import NavBar-> Navbar. updated package-lock.json with npm audit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container'
-import NavBar from 'react-bootstrap/NavBar'
-import Nav from 'react-bootstrap/Nav'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 const NavBarComp = () => {
   return (
-    <NavBar expand='md' className="bg-body-tertiary">
+    <Navbar expand='md' className='bg-body-tertiary'>
       <Container>
-        <NavBar.Brand to='/'>MyApp</NavBar.Brand>
-        <NavBar.Toggle aria-controls="basic-navbar-nav" />
-        <NavBar.Collapse id='basic-navbar-nav'>
+        <Navbar.Brand to='/'>MyApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/user'>Create</Nav.Link>
+            <Nav.Link as={Link} to='/'>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to='/user'>
+              Create
+            </Nav.Link>
           </Nav>
-        </NavBar.Collapse>
+        </Navbar.Collapse>
       </Container>
-    </NavBar>
-  )
-}
+    </Navbar>
+  );
+};
 
-export default NavBarComp
+export default NavBarComp;
